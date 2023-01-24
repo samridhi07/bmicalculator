77f5b8a19f09f220a85b17ed8b1934e3a872703e
samridhi.gupta <samridhigupta0707@gmail.com>
2023-01-24 18:23:32 +0530

    calculator done

---
+++ lib/constants.dart
@@ -0,0 +1,40 @@
+import 'package:flutter/material.dart';
+
+const bottomContainerHeight = 80.0;
+const activeColor = Color(0xff0a0e21);
+const inactiveColor = Color(0xff111328);
+const botttomContainerColor = Color(0xffeb1555);
+
+
+const labelText=  TextStyle(
+  fontSize: 18.0,
+  color: Color(0xff8d8e98),
+);
+
+const ktextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.w900,
+);
+const kLargeButtonTextStyle= TextStyle(
+  fontSize: 25.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kTitleTextStyle = TextStyle(
+  fontSize: 50.0,
+  fontWeight: FontWeight.bold,
+);
+
+const resultTextStyle = TextStyle(
+  color: Color(0xff24d876),
+  fontSize: 22.0,
+  fontWeight: FontWeight.bold,
+);
+
+const kBmiTextStyle= TextStyle(
+  fontSize: 100.0,
+  fontWeight: FontWeight.bold
+);
+const kBodyTextStyle= TextStyle(
+  fontSize: 22.0,
+);
